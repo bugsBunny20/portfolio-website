8162e4d3d9bad241b5ce1cb3ede29ebefb5bedc4
Arti Lanke <artilanke11@gmail.com>
2025-06-24 13:08:20 +0530

    Updating App and home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React from 'react';
 import Home from './pages/Home';
 import './index.css';
 
 function App() {
-   return (
-       <Router>
-           <Routes>
-               <Route path="/" element={<Home />} />
-           </Routes>
-       </Router>
-   );
+  return (
+    <>
+      <Home />
+    </>
+  );
 }
 
 export default App;
